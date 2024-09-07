bd3f076deb0d873d63df4ef5b2aa9b3817e9f47a
VEENA SURYAPRAKASH RAO <veenasrao5@gmail.com>
2024-09-07 18:07:20 +0530

    customer details

--- Backend/OnlineBankingSystem/src/main/java/com/project_14/OnlineBankingSystem/controller/DashboardController.java
+++ Backend/OnlineBankingSystem/src/main/java/com/project_14/OnlineBankingSystem/controller/DashboardController.java
@@ -1,18 +1,37 @@
 package com.project_14.OnlineBankingSystem.controller;
 
-import jakarta.servlet.http.HttpSession;
+import com.project_14.OnlineBankingSystem.model.Customer;
+import com.project_14.OnlineBankingSystem.repo.CustomerRepo;
+import com.project_14.OnlineBankingSystem.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/dashboard")
 public class DashboardController {
 
-    @GetMapping("/")
-    public String getCustomerDetails(HttpSession httpSession) {
-        httpSession.getAttribute("user");
-        return "";
+    private final DashboardService dashboardService;
+
+    @Autowired
+    public DashboardController(DashboardService dashboardService){
+        this.dashboardService = dashboardService;
     }
 
+    @GetMapping("/")
+    public ResponseEntity<Object> getCustomerDetails(@RequestParam String email) {
+        try{
+            Object response = dashboardService.getCustomerDetails(email);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Something went wrong!!");
+    }
 }
---
+++ Backend/OnlineBankingSystem/src/main/java/com/project_14/OnlineBankingSystem/service/DashboardService.java
@@ -0,0 +1,28 @@
+package com.project_14.OnlineBankingSystem.service;
+
+import com.project_14.OnlineBankingSystem.model.Customer;
+import com.project_14.OnlineBankingSystem.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DashboardService {
+
+    private final CustomerRepo customerRepo;
+
+    @Autowired
+    public DashboardService(CustomerRepo customerRepo){
+        this.customerRepo = customerRepo;
+    }
+
+    public Object getCustomerDetails(String email){
+        Optional<Customer> customerDetails = customerRepo.findByCustomerEmail(email);
+        if(customerDetails.isPresent()) {
+            return customerDetails.get();
+        }else {
+            return "Not Found";
+        }
+    }
+}
